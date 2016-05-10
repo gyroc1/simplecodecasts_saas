df1c89363c62592226a5e0f9c4a220f7e7a225a8
gyroc1 <quincym10@hotmail.com>
2016-05-10 02:38:23 +0000

    changed email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'quincym20@outlook.com'
+   default to: 'quincym10@hotmail.com'
    
    def contact_email(name, email, body)
        @name = name
